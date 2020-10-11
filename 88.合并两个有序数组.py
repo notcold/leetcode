05cb33ed8db4b979586e52c710d1bccf29b15a1f
史宽 <shikuan@tuya.com>
2020-10-11 19:30:19 +0800

    fix:easy q

---
+++ 88.合并两个有序数组.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=88 lang=python3
+#
+# [88] 合并两个有序数组
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        nums1[:] = sorted(nums1[:m] + nums2)
+
+# @lc code=end
+
