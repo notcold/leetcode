8650cedee201596f16682c3cb45b833ecf884b82
史宽 <shikuan@tuya.com>
2020-10-25 22:32:59 +0800

    fix

---
+++ 341.扁平化嵌套列表迭代器.py
@@ -0,0 +1,55 @@
+#
+# @lc app=leetcode.cn id=341 lang=python3
+#
+# [341] 扁平化嵌套列表迭代器
+#
+
+# @lc code=start
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+# class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self) -> [NestedInteger]:
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+
+class NestedIterator:
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.stack = []
+        self.recursion(nestedList)
+
+    def recursion(self, nestedList):
+        for i in range(len(nestedList) - 1, -1, -1):
+            if nestedList[i].isInteger():
+                self.stack.append(nestedList[i].getInteger())
+            else:
+                self.recursion(nestedList[i].getList())
+
+    def next(self):
+        return self.stack.pop()
+
+    def hasNext(self):
+        return len(self.stack) > 0
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
+# @lc code=end
