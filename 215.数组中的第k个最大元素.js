231f331c0bc272588ddae018546099b54f86543b
史宽 <shikuan@tuya.com>
2020-12-05 20:06:21 +0800

    fix:  K heap

---
+++ 215.数组中的第k个最大元素.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=javascript
+ *
+ * [215] 数组中的第K个最大元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+function maxHeapify(nums) {
+  nums.unshift(null);
+
+  for (let i = nums.length - 1; i >> 1 > 0; i--) {
+    // 自下往上堆化
+    if (nums[i] > nums[i >> 1]) {
+      // 如果子元素更大，则交换位置
+      const temp = nums[i];
+      nums[i] = nums[i >> 1];
+      nums[i >> 1] = temp;
+    }
+  }
+  nums.shift();
+  return nums[0];
+}
+var findKthLargest = function (nums, k) {
+  // heap klogn
+  let ret = null;
+  for (let i = 0; i < k; i++) {
+    ret = maxHeapify(nums);
+    nums.shift();
+  }
+  return ret;
+};
+// @lc code=end
